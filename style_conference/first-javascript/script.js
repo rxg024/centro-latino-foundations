13e2be10caee2205cadae8f8bb394b5baf2c88a0
rxg024 <rxg024@gmail.com>
2022-07-13 21:35:07 -0500

    Push my work after class of Wed 07-13-22 along with other pending corrections of previous homework

---
+++ style_conference/first-javascript/script.js
@@ -0,0 +1,187 @@
+// var student = "Jill";
+// console.log(student);
+// console.log("The name of the student is " + student);
+// console.log("student");
+// console.log(2);
+// console.log('2');
+// console.log("good" + "morning");
+// console.log("good" + " " + "morning");
+// console.log("good " + "morning");
+// console.log("good" + " morning");
+// var firstName = "Evan";
+// var lastName = "Henderson";
+// console.log(firstName + " " + lastName);
+// console.log(2 + 5);
+// console.log('2' + '5');
+// console.log(2 + '5');
+// console.log('2' + 5);
+// var cities = ["Oakland", "Chicago", "Pittsburgh"];
+// console.log(cities[1]);
+// console.log(cities[2]);
+// console.log(cities[0]);
+// console.log(cities[3]);
+// var points = [25, 36, 42, 81, 50];
+// console.log(points[4]);
+// var entries=["Steve", 25, "Bruce", true];
+// console.log(entries[1]);
+// console.log(entries[0]);
+// console.log(entries[3]);
+// console.log(cities);
+// console.log(cities.length);
+// cities.push("Omaha");
+// console.log(cities.length);
+// console.log(cities);
+// cities.push("Ames", "Beatrice", "Springfield");
+// console.log(cities.length);
+// console.log(cities);
+// cities.pop();
+// console.log(cities.length);
+// console.log(cities);
+// cities.splice(2,2);
+// console.log(cities);
+// cities.splice(1);
+// console.log(cities);
+var company = {
+    companyName: "AIM Institute",
+    federalTaxId: 84827690,
+    sections: [
+        {
+            sectionName: "AIM Code School",
+            instructors: [
+                {
+                    firstName: "Vanessa",
+                    lastName: "Kasun"
+                },
+                {
+                    firstName: "David",
+                    lastName: "Tarvin"
+                }
+            ],
+            numOFStudents: 26,
+            sectionHead: "Erin Lasiter"
+        },
+        {
+            sectionName: "Careerlink",
+            sectionHead: "Nate Decker"
+        }
+    ]
+}
+// console.log(company);
+// console.log("AIM's federal tax id is " + company.federalTaxId);
+// // console.log("Betty said, 'I made a great pie,' and asked usto taste it .");
+// console.log(company.sections[0]);
+// console.log(company.sections[0].instructors[1].);
+// var count = 1;
+// console.log('Count is now ' + count);
+// count = count + 5;
+// console.log('Count = count + 5 Count is now ' + count);
+// count += 3;
+// console.log('Count += 3 Count is now ' + count);
+// count++;
+// console.log('count++ Count is now ' + count);
+var x = 18;
+var y = 9;
+// console.log('18 divided by 9 = ' + (x / y));
+// console.log(x + 'divided by ' + y + ' = ' + (x / y));
+// console.log('The remainder of ' + x + ' divided by ' + y + ' is ' + (x % y));
+// y = 12;
+// console.log(x + 'divided by ' + y + ' = ' + (x / y));
+// console.log('The remainder of ' + x + ' divided by ' + y + ' is ' + (x % y));
+// x = 24;
+// console.log(x + 'divided by ' + y + ' = ' + (x / y));
+// console.log('The remainder of ' + x + ' divided by ' + y + ' is ' + (x % y));
+var x = 9;
+// console.log('is x == 8 true or false? ' + (x == 8));
+// console.log('is x == 9 true or false? ' + (x == 9));
+// console.log('is x == "9" true or false? ' + (x == "9"));
+// console.log('is x === 9 true or false? ' + (x === 9));
+// console.log('is x === "9" true or false? ' + (x === "9"));
+// console.log('is x != 8 true or false? ' + (x != 8));
+// console.log('is x != "8" true or false? ' + (x != "8"));
+// console.log('is x != 9 true or false? ' + (x != 9));
+// console.log('is x != "9" true or false? ' + (x != "9"));
+// console.log('is x !== "9" true or false? ' + (x !== "9"));
+// console.log('is x !== 9 true or false? ' + (x !== 9));
+// console.log('is x < 10 true or false? ' + (x < 10));
+// console.log('is x < 9 true or false? ' + (x < 9));
+// console.log('is x > 8 true or false? ' + (x > 8));
+// console.log('is x > 9 true or false? ' + (x > 9));
+// console.log('is x <= 9 true or false? ' + (x <= 9));
+// console.log('is x >= 9 true or false? ' + (x >= 9));
+// var goodDeal;
+// var toyCar = 5.00;
+// var goodDeal = (toyCar < 1.00) ? "Let's buy it!" : "Let's skip it.";
+// console.log(goodDeal);
+// var x = 15;
+// var y = 21;
+// console.log('x > 10 && y < 25: ' + (x > 10 && y < 25));
+// console.log('x > 20 && y < 25: ' + (x > 20 && y < 25));
+// console.log('x > 10 && y < 20: ' + (x > 10 && y < 20));
+// console.log('x > 20 && y < 20: ' + (x > 20 && y < 20));
+// console.log('========================');
+// console.log('x > 10 || y < 25: ' + (x > 10 || y < 25));
+// console.log('x > 20 || y < 25: ' + (x > 20 || y < 25));
+// console.log('x > 10 || y < 20: ' + (x > 10 || y < 20));
+// console.log('x > 20 || y < 20: ' + (x > 20 || y < 20));
+// console.log('#########################');
+// console.log('!(x > 20): ' + !(x > 20));
+// console.log('!(x < 20): ' + !(x < 20));
+// console.log('x > 10 && !(y < 25): ' + (x > 10 &&!(y < 25)));
+// console.log('!(x > 20 && y < 20): ' + !(x > 20 && y < 20));
+
+// var zombies = 28;
+// if (zombies < 20) {
+//     console.log("AAARGH! Too many zombies!!!");
+// }
+
+// var zombies = 28;
+// if (zombies > 30) {
+//     console.log("AAARGH! Too many zombies!!!");
+// }
+
+// if (condition) {
+//     // do something
+// } else {
+//     // do something else
+// }
+
+// zombies = 10;
+// if (zombies > 20) {
+//     console.log("AAARGH! Too many zombies!!!");
+// } else {
+//     console.log("Glad there are not too many zombies.");
+// }
+
+// if (zombies == 0) {
+//     console.log("No zombies here.");
+// } else (zombies <= 10) {
+//     console.log("Not too many zombies here.");
+// } else (zombies <= 20)  {
+//     console.log("I'm getting scared!");
+// } else {
+//     console.log("ZOMBIES!!!");
+// }
+
+var grade = 90;
+
+if (grade <= 90) {
+    console.log('You got an A');
+// } else if (grade >= 80 && grade <= 89)
+} else if (grade <= 80) {
+    console.log("You got an B");
+} else if (grade >= 70) {
+    console.log("You got an C");
+} else if (grade >= 60) {
+    console.log("You got an D");
+} else { 
+    console.log("FAILURE!!! My son is a DONUT!");
+}
+
+var goldCoins = 29;
+if (goldCoins > 5) {
+    console.log("Cool, a bit of change!");
+} else if (goldCoins > 10) {
+    console.log("Great! A lot of change");
+} else if (goldCoins > 20) {
+    console.log("I'm rich!!!");
+}
